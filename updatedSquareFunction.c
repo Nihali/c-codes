ade6057292fb2fd196cef9e43fddffc04feacfec
Nihali <nihalisawant20@gmail.com>
2019-10-30 13:25:22 +0530

    Add files via upload

---
+++ updatedSquareFunction.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//declaring a function square above the main function
+int square(int num);
+
+int main() {
+
+    int result,n;
+    n=6;
+
+    for(int i=1;i<n;i++)
+    {
+        result=square(i);
+        printf("The result is: %d\n",square(i));
+
+    }
+
+	return 0;
+}
+
+int square(int num)
+{
+    int y;
+
+    y=num*num;
+    return(y);
+}
